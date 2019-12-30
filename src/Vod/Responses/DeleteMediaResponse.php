50417e962bd2265cf5db0ed1b1693d1ebc6ebcf9
张超君 <380993157@qq.com>
2019-12-30 14:53:51 +0800

    fix: 补全删除媒体的Response

---
+++ src/Vod/Responses/DeleteMediaResponse.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BiuBiuJun\QCloud\Vod\Responses;
+
+use BiuBiuJun\QCloud\Kernel\BaseTcResponse;
+
+/**
+ * Class DeleteMediaResponse
+ *
+ * @package BiuBiuJun\QCloud\Vod\Responses
+ */
+class DeleteMediaResponse extends BaseTcResponse
+{
+}
